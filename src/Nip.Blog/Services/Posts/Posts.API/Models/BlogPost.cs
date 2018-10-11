c58bfc1ff11a8b7daf58f2ddbd6926ba3c2fc9ec
Wit <witalian.zajac@gmail.com>
2018-10-11 14:33:39 +0200

    Added data annotations to the BlogPost model

--- src/Nip.Blog/Services/Posts/Posts.API/Models/BlogPost.cs
+++ src/Nip.Blog/Services/Posts/Posts.API/Models/BlogPost.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,13 @@ namespace Nip.Blog.Services.Posts.API.Models
     public class BlogPost
     {
         public long Id { get; set; }
+
+        [Required]
+        [StringLength(32, MinimumLength = 3)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$", ErrorMessage = "Should start from capital letter and consist only of basic characters.")]
         public string Title { get; set; }
+
+        [StringLength(4096)]
         public string Description { get; set; }
     }
 }
