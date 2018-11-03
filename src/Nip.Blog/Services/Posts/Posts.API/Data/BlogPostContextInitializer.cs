e4f6d3b63186906e8bd4f616264c4399cbdec8f9
Wit <witalian.zajac@gmail.com>
2018-11-03 15:59:39 +0100

    Added db initialization when in develoment mode

---
+++ src/Nip.Blog/Services/Posts/Posts.API/Data/BlogPostContextInitializer.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Nip.Blog.Services.Posts.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Nip.Blog.Services.Posts.API.Data
+{
+    public class BlogPostContextInitializer
+    {
+        public static void Initialize(BlogPostContext context, IHostingEnvironment env)
+        {
+            if (!env.IsDevelopment())
+            {
+                // Seed only in development environment
+                return;
+            }
+
+            context.Database.EnsureCreated();
+
+            if (context.BlogPosts.Any())
+            {
+                // DB has been seeded
+                return;
+            }
+
+            var posts = new List<BlogPost>
+            {
+                new BlogPost{Title="Test 1", Description = "Descrition 1"},
+                new BlogPost{Title="Test 2", Description = "Descrition 2"},
+                new BlogPost{Title="Test 3", Description = "Descrition 3"},
+                new BlogPost{Title="Test 4", Description = "Descrition 4"},
+                new BlogPost{Title="Test 5", Description = "Descrition 5"},
+            };
+
+            context.BlogPosts.AddRange(posts);
+            context.SaveChanges();
+        }
+    }
+}
--- src/Nip.Blog/Services/Posts/Posts.API/Program.cs
+++ src/Nip.Blog/Services/Posts/Posts.API/Program.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Nip.Blog.Services.Posts.API.Data;
 
 namespace Nip.Blog.Services.Posts.API
 {
@@ -14,7 +16,7 @@ namespace Nip.Blog.Services.Posts.API
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args).Build().InitializeDatabase().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
@@ -26,5 +28,29 @@ namespace Nip.Blog.Services.Posts.API
                     logging.AddConsole();
                     logging.AddFile("Logs/blogposts-{Date}.log");
                 });
+    }
+
+    internal static class ConfigureWebHostHelper
+    {
+        public static IWebHost InitializeDatabase(this IWebHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var env = services.GetRequiredService<IHostingEnvironment>();
+                    var context = services.GetRequiredService<BlogPostContext>();
+                    BlogPostContextInitializer.Initialize(context, env);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            return host;
+        }
     }
 }
